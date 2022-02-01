5fb8135ea42521e831cf238826c47fc9f49369e8
logica <84759675+logica0419@users.noreply.github.com>
2022-02-02 00:51:01 +0900

    added timezone settings

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Q-n-A/Q-n-A/cmd"
 )
+
+// タイムゾーンの設定
+func init() {
+	const location = "Asia/Tokyo"
+
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+
+	time.Local = loc
+}
 
 func main() {
 	err := cmd.Execute()
